e1b504be93b0a66ef702e11a0445725b165e705c
15001AQ <48032391+15001AQ@users.noreply.github.com>
2019-03-13 10:04:23 +1300

    v1.2
    updated and managed to plug in sub windows. now can display the question.

---
+++ 20190307_enviroschoolQuizv1.0.py
@@ -0,0 +1,57 @@
+from appJar import gui
+
+questionList = []
+
+app = gui("Environment Quiz","500x200") 
+
+class Question:
+    def __init__(self,quesText,cans,pans1,pans2,pans3):
+        self.quesText = quesText
+        self.cans = cans
+        self.pans1 = pans1
+        self.pans2 = pans2
+        self.pans3 = pans3
+        
+    def show(self):
+        app.setLabel("Questions",self.quesText)
+        
+  
+#main routine
+def launch(win):
+    if win == "Start":
+        app.showSubWindow("question 1")
+        app.setLabel("Questions",questionList[0].show())
+        app.getEntry("Your Answer:")
+        if win == "Enter Answer":
+            app.showSubWindow("correct")
+    elif win == "Exit":
+        app.stop()
+    
+#main routine - setting up the GUI
+app.addLabel("title", "Welcome to my Environment Quiz")
+app.addLabel("belowtitle", "In this quiz, you will be asked various questions\n        on the problems with our environment")
+app.addLabel("belowbelowtitle", "Press Start to begin.")
+app.setBg("light green")
+app.addButtons(["Start", "Exit"],launch)
+
+#CREATING SUB WINDOW FOR QUESTIONS
+app.startSubWindow("question 1", modal=True)
+app.setBg("light green")
+app.setSize(400,400)
+app.addLabel("Questions", "")
+app.addLabelEntry("Your Answer:")
+app.addButtons(["Enter answer", "Go back"],launch)
+app.stopSubWindow()
+
+app.startSubWindow("correct")
+app.setBg("red")
+app.addLabel("f", "f")
+app.stopSubWindow()
+
+
+questionList.append(Question("How long?","7","3","2","5"))
+questionList.append(Question("How high?","7","3","2","5"))
+
+questionList[0].show()
+
+app.go()
