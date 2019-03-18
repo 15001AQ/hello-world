713f9c9f37a0c69a2fbd768bf5fca8b46b583029
15001AQ <48032391+15001AQ@users.noreply.github.com>
2019-03-19 10:57:20 +1300

    begun to figure out how to swap questions
    using x = level

--- 20190307_enviroschoolQuizv1.0.py
+++ 20190307_enviroschoolQuizv1.0.py
@@ -1,5 +1,6 @@
 from appJar import gui
 
+level = 0
 questionList = []
 
 app = gui("Environment Quiz","500x200") 
@@ -19,19 +20,25 @@ class Question:
 #main routine
 def launch(win):
     if win == "Start":
-        app.showSubWindow("question 1")
-        app.setLabel("Questions",questionList[0].show())
-        app.getEntry("Your Answer:")
-        
+        app.showSubWindow("questions")
+        x = level
+        app.setLabel("Questions",questionList[x].show())        
     elif win == "Exit":
         app.stop()
+
     
 def check(button):
     if button == "Enter Answer":
-        correct()
+        correctOrNot()
     elif button == "Go back":
         app.stop()
-    
+
+def correctOrNot():
+    app.getOptionBox("Your Answer:")
+    if app.getOptionBox("Your Answer:") == "sewage":
+        app.showSubWindow("correct")
+    else:
+        pass
         
 #main routine - setting up the GUI
 app.addLabel("title", "Welcome to my Environment Quiz")
@@ -41,23 +48,22 @@ app.setBg("light green")
 app.addButtons(["Start", "Exit"],launch)
 
 #CREATING SUB WINDOW FOR QUESTIONS
-app.startSubWindow("question 1", modal=True)
+app.startSubWindow("questions", modal=True)
 app.setBg("light green")
 app.setSize(400,400)
 app.addLabel("Questions", "")
-app.addLabelEntry("Your Answer:")
+app.addLabelOptionBox("Your Answer:",["sewage","oil spills","acid rain","rubbish"])
+
 app.addButtons(["Enter answer", "Go back"],check)
 app.stopSubWindow()
 
-app.startSubWindow("correct")
+app.startSubWindow("correct",modal=True)
 app.setBg("red")
-app.addLabel("f", "f")
+app.addLabel("titl", "fgrehsd")
 app.stopSubWindow()
 
 
 questionList.append(Question("What is water pollution caused by?","sewage","oil spills","acid rain","rubbish"))
-questionList.append(Question("How high?","7","3","2","5"))
-
-questionList[0].show()
+questionList.append(Question("What is causing global warming?","7","3","2","5"))
 
 app.go()
